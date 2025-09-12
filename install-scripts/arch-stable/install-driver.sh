cbd1c574266182c96964db3b65ea3c26817bb372
VoktexYT <ubguertin@gmail.com>
2025-09-12 13:31:34 -0400

    Fix bug in script

--- install-scripts/arch-stable/install-driver.sh
+++ install-scripts/arch-stable/install-driver.sh
@@ -1,24 +1,24 @@
-sudo pacman -Sy -noconfirm
+sudo pacman -Sy --noconfirm
 
-sudo pacman -S -noconfirm base-devel
-sudo pacman -S -noconfirm xf86-video-intel mesa lib32-mesa
-sudo pacman -S -noconfirm intel-media-driver libva-intel-driver
-sudo pacman -S -noconfirm alsa-utils pulseaudio pulseaudio-alsa
-sudo pacman -S -noconfirm networkmanager wpa_supplicant
+sudo pacman -S --noconfirm base-devel
+sudo pacman -S --noconfirm xf86-video-intel mesa lib32-mesa
+sudo pacman -S --noconfirm intel-media-driver libva-intel-driver
+sudo pacman -S --noconfirm alsa-utils pulseaudio pulseaudio-alsa
+sudo pacman -S --noconfirm networkmanager wpa_supplicant
 
 sudo systemctl enable NetworkManager
 
-sudo pacman -S -noconfirm acpi acpid thinkfan
+sudo pacman -S --noconfirm acpi acpid thinkfan
 
 sudo systemctl enable acpid
 
-sudo pacman -S -noconfirm tp_smapi tlp
+sudo pacman -S --noconfirm tp_smapi tlp
 
 sudo systemctl enable tlp
 
-sudo pacman -S -noconfirm xf86-input-synaptics xf86-input-libinput
-sudo pacman -S -noconfirm b43-fwcutter
-sudo pacman -S -noconfirm fprintd
+sudo pacman -S --noconfirm xf86-input-synaptics xf86-input-libinput
+sudo pacman -S --noconfirm b43-fwcutter
+sudo pacman -S --noconfirm fprintd
 
 sudo modprobe -a thinkpad_acpi tp_smapi
 lspci -k
