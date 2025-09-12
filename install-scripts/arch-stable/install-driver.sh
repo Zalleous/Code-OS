55331bc9bf847648e6daf969121f8d87d517ef05
VoktexYT <ubguertin@gmail.com>
2025-09-12 12:59:44 -0400

    Update driver installation file

--- install-scripts/arch-stable/install-driver.sh
+++ install-scripts/arch-stable/install-driver.sh
@@ -1,8 +1,10 @@
+sudo pacman -Sy
+sudo pacman -S base-devel
 sudo pacman -S xf86-video-intel mesa lib32-mesa
 sudo pacman -S intel-media-driver libva-intel-driver
 sudo pacman -S alsa-utils pulseaudio pulseaudio-alsa
 sudo pacman -S networkmanager wpa_supplicant
-sudo systemctl enable networkmanager
+sudo systemctl enable NetworkManager
 sudo pacman -S acpi acpid thinkfan
 sudo systemctl enable acpid
 sudo pacman -S tp_smapi tlp
@@ -10,7 +12,6 @@ sudo systemctl enable tlp
 sudo pacman -S xf86-input-synaptics xf86-input-libinput
 sudo pacman -S b43-fwcutter
 sudo pacman -S fprintd
-
 sudo modprobe -a thinkpad_acpi tp_smapi
 lspci -k
 lsusb
