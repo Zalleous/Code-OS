0440f48940095e1a9532814b9cf50d8f295eefa5
Zalleous <shaadwz@gmail.com>
2025-09-16 13:37:05 -0400

    Modified driver-installed to use seperate .txt folder

--- install-scripts/arch-stable/install-driver.sh
+++ install-scripts/arch-stable/install-driver.sh
@@ -1,32 +1,12 @@
 sudo pacman -Sy --noconfirm
 
-sudo pacman -S --noconfirm base-devel
-sudo pacman -S --noconfirm xf86-video-intel 
-sudo pacman -S --noconfirm mesa lib32-mesa
-sudo pacman -S --noconfirm intel-media-driver 
-sudo pacman -S --noconfirm libva-intel-driver
-sudo pacman -S --noconfirm alsa-utils 
-sudo pacman -S --noconfirm pulseaudio 
-sudo pacman -S --noconfirm pulseaudio-alsa
-sudo pacman -S --noconfirm networkmanager 
-sudo pacman -S --noconfirm wpa_supplicant
+xargs -r sudo pacman -S --noconfirm --needed < packages.txt
 
 sudo systemctl enable NetworkManager
 
-sudo pacman -S --noconfirm acpi
-sudo pacman -S --noconfirm acpid 
-sudo pacman -S --noconfirm thinkfan
-
 sudo systemctl enable acpid
 
-sudo pacman -S --noconfirm tp_smapi tlp
-
 sudo systemctl enable tlp
-
-sudo pacman -S --noconfirm xf86-input-synaptics 
-sudo pacman -S --noconfirm xf86-input-libinput
-sudo pacman -S --noconfirm b43-fwcutter
-sudo pacman -S --noconfirm fprintd
 
 sudo modprobe -a thinkpad_acpi tp_smapi
 lspci -k
